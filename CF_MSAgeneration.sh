ded099fad24fec4963e123df1f284b50cc43d11e
Louis <carrelbl@mc-login1.mesu>
2024-08-20 14:57:38 +0200

    test2

--- CF_MSAgeneration.sh
+++ CF_MSAgeneration.sh
@@ -3,5 +3,14 @@
 #SBATCH -N 1
 #SBATCH -n 16 
 
+
 module load colabfold/1.5.2/gcc
+
+
+
+export X=/scratch/carrelbl/Alt
+export RESULT_FOLDER=/scratch/carrelbl/
+
+
+
 colabfold_search  --threads=32 --db-load-mode=2 $X.fasta  ${COLABFOLD_DB} $RESULT_FOLDER
